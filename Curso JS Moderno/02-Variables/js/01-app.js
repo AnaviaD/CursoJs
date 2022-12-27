9acb52db7e2d63066f0c774e0999d38e39056ff3
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2022-12-27 16:53:46 -0600

    Finish first lession

---
+++ Curso JS Moderno/02-Variables/js/01-app.js
@@ -0,0 +1,31 @@
+// Inincializar una variable con un valor
+var producto = 'Monitor de 24 pulgadas'
+console.log(producto)
+
+// Las variables se pueden reasignar
+producto = 'Monitor de 19 pulgadas'
+console.log(producto)
+
+// Javascript es un lenguaje de tipo dinamico, no se especifica tipo de dato
+producto = 20
+console.log(producto)
+
+// Se puede inicializar sin valor y asignarlo despues
+var disponible
+disponible = true
+
+disponible = false
+
+// Inicializar multiples variables
+var categoria = 'Computadoras', marca = 'Marca Famosa', calificacion = 5
+
+/*
+las variables no pueden ser inicializadas con numeros
+var 99dias          --va a marcar error
+var 01Usuario       --va a marcar error
+var _01Usuario      Este si se puede
+*/
+
+var nombreProducto      //camelCase
+var nombre_producto
+var NombreProducto
