c12bf7099cb4eead6c030cf240406ef0553ded3c
Enrique Alberto Navia <anavia_n@outlook.com>
2022-12-28 12:47:24 -0600

    Add the first 2 examples of string of the course

---
+++ Curso JS Moderno/03-Strings/js/02-app.js
@@ -0,0 +1,13 @@
+//Metodos para cadenas de string
+const productos = 'Monitor de 20 Pulgadas';
+
+console.log(productos);
+
+//Conocer la cantidad de letras de la cadena de texto
+console.log(productos.length);
+
+console.log(productos.indexOf('Monitor'));
+console.log(productos.indexOf('Pulgadas'));
+
+//Nos dice si es que existe la palabra esta en el string
+console.log(productos.includes('Monitor'));
