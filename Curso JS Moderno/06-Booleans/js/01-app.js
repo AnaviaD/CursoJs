a28cb400ec9f422ecf8334074e7a2d887fce77cb
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2022-12-30 16:24:47 -0600

    Finish Booleans section SIUUUUUUUU

---
+++ Curso JS Moderno/06-Booleans/js/01-app.js
@@ -0,0 +1,14 @@
+const boolean01 = true;
+const boolean02 = false;
+const boolean03 = "true";
+
+// console.log(boolean01)
+// console.log(boolean01)
+
+// console.log(typeof boolean01);
+
+console.log(boolean01 === boolean03);
+
+
+const boolean04 = new Boolean(true);
+console.log(typeof boolean04);
