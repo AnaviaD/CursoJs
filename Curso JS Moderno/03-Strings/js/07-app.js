ce4aec2cf75a96c6724d605ef7cc1b3d67bc2d8e
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2022-12-30 13:25:59 -0600

    Add final chapter strings

---
+++ Curso JS Moderno/03-Strings/js/07-app.js
@@ -0,0 +1,15 @@
+// Convertir de minus a mayus 
+
+const producto06 = 'Monitor 20 pulgadas ';
+
+console.log(producto06.toUpperCase())
+console.log(producto06.toLowerCase())
+
+const email = 'CORREO@CORREO.com';
+
+console.log(email.toLowerCase())
+
+const precio = 300;
+console.log(precio)
+console.log(precio.toString())
+// Elresultado es el mismo
