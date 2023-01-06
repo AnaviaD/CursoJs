8a934b770d8a671b1fbe327150e3113c290610ac
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-06 13:15:40 -0600

    Add the first 5 examples of objects

---
+++ Curso JS Moderno/07-Objetos/js/05-app.js
@@ -0,0 +1,20 @@
+// Objetos dentro de otros objetos
+// Objetos anidados 
+
+const producro005 = {
+    nombre0001      :   "Monitor 20 pulgadas",
+    precio00001     :   300,
+    disponible0001  :   false,
+    informacion001  :   {
+        medidas     :   {
+            peso        :   '1Kg',
+            medida      :   '1m'
+        },
+        fabricacion     :{
+            pais        :   'China'
+        }
+    }
+}
+
+
+console.log(producro005);
