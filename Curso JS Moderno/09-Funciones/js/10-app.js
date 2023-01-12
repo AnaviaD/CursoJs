66a1ef1387017b1c51c030c3d8d89f74611dc52f
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-12 17:58:51 -0600

    Learn arrow functions

---
+++ Curso JS Moderno/09-Funciones/js/10-app.js
@@ -0,0 +1,18 @@
+// // Los arrowfunction tambien como return todo lo que se ejecute dentro de el
+// const aprendiendo0010 = function(){
+//     console.log('aprendiendo Javascript');
+// }
+
+// const aprendiendo00102 = () => {
+//     console.log('aprendiendo Javascript X2');
+// }
+
+// const aprendiendo00103 = () => console.log('aprendiendo Javascript X3');
+
+// // Por lo que podemos hacer esto del console log
+
+// console.log(aprendiendo00103())
+
+// aprendiendo00103()
+// aprendiendo00102()
+// aprendiendo0010()
