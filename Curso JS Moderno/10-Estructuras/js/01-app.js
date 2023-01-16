612e5128b50042af0e3ef52a70d47875b25eeccd
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-16 17:24:36 -0600

    Add first 3 exercises

---
+++ Curso JS Moderno/10-Estructuras/js/01-app.js
@@ -0,0 +1,6 @@
+// const puntake = 1001;
+
+// if(puntake == 1000)
+// {
+//     console.log("Sigue igual...");
+// }
---
+++ Curso JS Moderno/10-Estructuras/js/02-app.js
@@ -0,0 +1,11 @@
+// const puntaje002 = 1000;
+// const puntaje2002 = "1000"
+
+// console.log(typeof puntaje002)
+// console.log(typeof puntaje2002)
+
+// if (puntaje002 !== "1000") {
+//     console.log('Si es diferente!')
+// }else{
+//     console.log(`No es diferente`)
+// }
