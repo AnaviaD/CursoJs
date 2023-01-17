330fa353ee750aff0a1f52303ed7aca9c7dfb7c9
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-17 17:37:12 -0600

    Add fizz buzz de dowhile

---
+++ Curso JS Moderno/11-Iteradores/js/05-app.js
@@ -0,0 +1,16 @@
+// let i = 1;
+
+// do{
+//     if (i > 0) {
+//         if ((i % 3) == 0) {
+//             console.log(`${i} fizz`)
+//         }
+//         if ((i%5) == 0) {
+//             console.log(`${i} buzz`)
+//         }
+//         if ((i%15) == 0) {
+//             console.log(`${i} FIZZBUZZ`)
+//         }
+//     }
+//     i++;
+// }while(i < 100);
