dc60cf5f408e7f5fb3e86a6c3e5c86ec71d89f63
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-17 17:33:12 -0600

    FIZZBUZZ added

---
+++ Curso JS Moderno/11-Iteradores/js/03-app.js
@@ -0,0 +1,34 @@
+// // Fizz buzz
+
+// // 3 6 9 12 ...         Fizz
+// // 5 10 15 20 ...       buzz
+// // 15 30 45 ...         FIZZBUZZ!
+
+
+// for (let i = 0; i < 50; i++) {
+//     if (i > 0) {
+//         if ((i % 3) == 0) {
+//             console.log(`${i} fizz`)
+//         }
+//         if ((i%5) == 0) {
+//             console.log(`${i} buzz`)
+//         }
+//         if ((i%15) == 0) {
+//             console.log(`${i} FIZZBUZZ`)
+//         }
+//     }
+// }
+
+
+// console.log("========================================")
+// for (let i = 0; i < 50; i++) {
+//     if (i > 0) {
+//         if ((i%15) == 0) {
+//             console.log(`${i} FIZZBUZZ`)
+//         }else if ((i % 3) == 0) {
+//             console.log(`${i} fizz`)
+//         }else if ((i%5) == 0) {
+//             console.log(`${i} buzz`)
+//         }
+//     }
+// }
