f8a058a9f3756f7a32e9b0cc77c5aad3d9663893
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-16 18:05:15 -0600

    Repasar el ultimo loop continue y break

---
+++ Curso JS Moderno/11-Iteradores/js/02-app.js
@@ -0,0 +1,25 @@
+for (let i = 0; i <= 10; i++) {
+    if (i === 5) {
+        console.log(`CINCO`);
+        break;
+        // continue;
+    }
+    console.log(`Numero: ${i}`)
+}
+
+const carrito009 = [
+    {nombre: 'Monitor 27 Pulgadas', precio: 500},
+    {nombre: 'Television',          precio: 100},
+    {nombre: 'Tablet',              precio: 200, descuento: true},
+    {nombre: 'Audifonos',           precio: 300},
+    {nombre: 'Teclado',             precio: 400},
+    {nombre: 'Celular',             precio: 700}
+]
+
+for (let i = 0; i < carrito009.length; i++) {
+    if (carrito009[i].descuento) {
+        console.log(`El articulo ${carrito009[i].nombre} Tiene descuento`);
+        continue;
+    }
+    console.log(carrito009[i].nombre)
+}
