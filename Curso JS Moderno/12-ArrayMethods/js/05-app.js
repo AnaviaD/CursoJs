c9c75464e0090a792f745f17fe498aa7eea2a01a
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-19 17:51:35 -0600

    Every and some

--- Curso JS Moderno/12-ArrayMethods/js/05-app.js
+++ Curso JS Moderno/12-ArrayMethods/js/05-app.js
@@ -1,21 +1,21 @@
-const carrito = [
-    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
-    { nombre: 'Televisión', precio: 100 },
-    { nombre: 'Tablet', precio: 200 },
-    { nombre: 'Audifonos', precio: 300 },
-    { nombre: 'Teclado', precio: 400 },
-    { nombre: 'Celular', precio: 700 },
-]
+// const carrito = [
+//     { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+//     { nombre: 'Televisión', precio: 100 },
+//     { nombre: 'Tablet', precio: 200 },
+//     { nombre: 'Audifonos', precio: 300 },
+//     { nombre: 'Teclado', precio: 400 },
+//     { nombre: 'Celular', precio: 700 },
+// ]
 
-let resultado = '';
+// let resultado = '';
 
-carrito.forEach((producto, index) => {
-    if (producto.nombre == 'Tablet') {
-        resultado = carrito[index]
-    }
-});
+// carrito.forEach((producto, index) => {
+//     if (producto.nombre == 'Tablet') {
+//         resultado = carrito[index]
+//     }
+// });
 
-console.log(resultado)
+// console.log(resultado)
 
-const resultado02 = carrito.find( producto => producto.precio == 100)
-console.log(resultado02)
+// const resultado02 = carrito.find( producto => producto.precio == 100)
+// console.log(resultado02)
