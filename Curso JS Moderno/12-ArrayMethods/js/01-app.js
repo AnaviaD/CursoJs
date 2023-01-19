5ee3331e5143645ad98c895e36a60153a4971c5a
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-19 17:29:15 -0600

    Add first exercise of js

--- Curso JS Moderno/12-ArrayMethods/js/01-app.js
+++ Curso JS Moderno/12-ArrayMethods/js/01-app.js
@@ -1,30 +1,30 @@
-const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+// const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
 
-const carrito = [
-    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
-    { nombre: 'Televisión', precio: 100 },
-    { nombre: 'Tablet', precio: 200 },
-    { nombre: 'Audifonos', precio: 300 },
-    { nombre: 'Teclado', precio: 400 },
-    { nombre: 'Celular', precio: 700 },
-]
+// const carrito = [
+//     { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+//     { nombre: 'Televisión', precio: 100 },
+//     { nombre: 'Tablet', precio: 200 },
+//     { nombre: 'Audifonos', precio: 300 },
+//     { nombre: 'Teclado', precio: 400 },
+//     { nombre: 'Celular', precio: 700 },
+// ]
 
-// Comprobar si existe el valor
-meses.forEach((mes) =>{
-    if (mes === 'Enero') {
-        console.log(`Enero si existe`)
-    }
-});
+// // Comprobar si existe el valor
+// meses.forEach((mes) =>{
+//     if (mes === 'Enero') {
+//         console.log(`Enero si existe`)
+//     }
+// });
 
-// Include solo funciona con arreglo de un solo indice
-const resultado = meses.includes('Enero');
-console.log(resultado)
+// // Include solo funciona con arreglo de un solo indice
+// const resultado = meses.includes('Enero');
+// console.log(resultado)
 
-const existe = carrito.some((prod) => {
-    return prod.nombre == 'Celular'
-})
+// const existe = carrito.some((prod) => {
+//     return prod.nombre == 'Celular'
+// })
 
-console.log(existe)
+// console.log(existe)
 
-const existe2 = meses.some(m =>  m === 'Febrero')
-console.log(existe2)
+// const existe2 = meses.some(m =>  m === 'Febrero')
+// console.log(existe2)
---
+++ Curso JS Moderno/12-ArrayMethods/js/02-app.js
@@ -0,0 +1,24 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Televisión', precio: 100 },
+    { nombre: 'Tablet', precio: 200 },
+    { nombre: 'Audifonos', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+]
+
+// Comprobar si existe el valor
+meses.forEach((mes, i) =>{
+    if (mes == 'Enero') {
+        console.log(`Encontrado el indice ${i}`)
+    }
+});
+
+const indice = meses.findIndex(mes => mes === 'Diciembre')
+console.log(indice)
+// Cuando retorna -1 es que no lo encontro
+
+const indice02 = carrito.findIndex( producto => producto.precio == 300)
+console.log(indice02)
