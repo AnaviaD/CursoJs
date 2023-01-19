2d3efa5f4b8f3ebe921c9d858d803de77a32a822
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-19 17:56:30 -0600

    concat y ...

---
+++ Curso JS Moderno/12-ArrayMethods/js/07-app.js
@@ -0,0 +1,10 @@
+// const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+// const meses2 = ['Agosto', 'Septiembre'];
+// const meses3 = ['Octubre', 'Noviembre', 'Diciembre']
+
+// const resultado = meses.concat(meses2, meses3)
+
+// console.log(resultado)
+
+// const resultado02 = [...meses3, ...meses, ...meses2]
+// console.log(resultado02)
