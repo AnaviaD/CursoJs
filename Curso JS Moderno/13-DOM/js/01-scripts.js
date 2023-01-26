8725d696672663badd04cad060fa4f46e83de4c8
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-25 18:01:31 -0600

    Add second class getelementByClassName

--- Curso JS Moderno/13-DOM/js/01-scripts.js
+++ Curso JS Moderno/13-DOM/js/01-scripts.js
@@ -1,24 +1,24 @@
-let elemento001;
+// let elemento001;
 
-elemento001 = document;
-elemento001 = document.all;
-elemento001 = document.head;
-elemento001 = document.body;
-elemento001 = document.domain;
-elemento001 = document.forms;
-elemento001 = document.forms[0];
-elemento001 = document.forms[0].method;
-elemento001 = document.forms[0].classList;
-elemento001 = document.forms[0].action;
+// elemento001 = document;
+// elemento001 = document.all;
+// elemento001 = document.head;
+// elemento001 = document.body;
+// elemento001 = document.domain;
+// elemento001 = document.forms;
+// elemento001 = document.forms[0];
+// elemento001 = document.forms[0].method;
+// elemento001 = document.forms[0].classList;
+// elemento001 = document.forms[0].action;
 
-elemento001 = document.links;
-elemento001 = document.links[0];
-elemento001 = document.links[4];
-elemento001 = document.links[4].classList;
-elemento001 = document.links[4].className;
+// elemento001 = document.links;
+// elemento001 = document.links[0];
+// elemento001 = document.links[4];
+// elemento001 = document.links[4].classList;
+// elemento001 = document.links[4].className;
 
-elemento001 = document.images;
+// elemento001 = document.images;
 
-elemento001 = document.scripts;
+// elemento001 = document.scripts;
 
-console.log(elemento001)
+// console.log(elemento001)
