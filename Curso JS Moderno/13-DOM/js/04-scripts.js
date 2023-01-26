0653a8d62f2980437e8de125dc1687633bf5068a
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-26 17:51:22 -0600

    Add lesson 4 to 6

--- Curso JS Moderno/13-DOM/js/04-scripts.js
+++ Curso JS Moderno/13-DOM/js/04-scripts.js
@@ -1,1 +1,13 @@
 // const card = document.querySelector('.card');
+// console.log(card);
+
+// // Podemos tener selectores especificos como en CSS
+// const info = document.querySelector('.premium .info')
+// console.log(info)
+
+// const segundoCard = document.querySelector('section.hospedaje .card:nth-child(2)');
+// console.log(segundoCard)
+
+// // Seleccionar el formulario
+// const formulario = document.querySelector('.contenido-heroe #formulario');
+// console.log(formulario)
