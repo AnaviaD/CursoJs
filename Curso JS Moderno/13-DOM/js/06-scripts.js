11516b36d330d570b807cf2cff7ff4109911efad
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-30 17:32:28 -0600

    finish the 6-7 class
    add class by selectQuery command

--- Curso JS Moderno/13-DOM/js/06-scripts.js
+++ Curso JS Moderno/13-DOM/js/06-scripts.js
@@ -7,3 +7,8 @@
 
 // const nuevoHeading = 'Nuevo heading'
 // document.querySelector('.contenido-hero h1').textContent = nuevoHeading;
+
+// const imagen = document.querySelector('.card img')
+// console.log(imagen)
+// Gracias a la propiedad del src se puede modificar el source code y cambiar la imagen desd el codigo
+// imagen.src = 'img/hacer2.jpg'
