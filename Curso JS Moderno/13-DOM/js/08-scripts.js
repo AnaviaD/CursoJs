a380e44c15bed641c740839057ea0b49070aaeb4
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-30 17:41:39 -0600

    children & parent element

---
+++ Curso JS Moderno/13-DOM/js/08-scripts.js
@@ -0,0 +1,15 @@
+// const navegacion = document.querySelector('.navegacion')
+
+// console.log(navegacion)
+// console.log(navegacion.childNodes)
+// console.log(navegacion.children)
+
+
+// console.log(navegacion.children[1].nodeName)
+// console.log(navegacion.children[1].nodeType)
+
+// const card = document.querySelector('.card')
+
+// card.children[1].children[1].textContent = 'Nuevo heading desde codigo'
+
+// console.log(card.children[1].children[1].textContent)
