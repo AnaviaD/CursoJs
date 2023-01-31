777a8c131ab8e52cfe63769bd0e5427f8552f9de
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-30 18:16:47 -0600

    comment class 10

--- Curso JS Moderno/13-DOM/js/10-scripts.js
+++ Curso JS Moderno/13-DOM/js/10-scripts.js
@@ -1,17 +1,17 @@
-const enlace = document.createElement('A')
-enlace.textContent = 'Nuevo Enlace';
-enlace.href = '/nuevo-enlace';
+// const enlace = document.createElement('A')
+// enlace.textContent = 'Nuevo Enlace';
+// enlace.href = '/nuevo-enlace';
 
-console.log(enlace)
+// console.log(enlace)
 
-enlace.target = "_blank"
+// enlace.target = "_blank"
 
-enlace.setAttribute('data-enlace', 'nuevo-enlace')
+// enlace.setAttribute('data-enlace', 'nuevo-enlace')
 
-enlace.classList.add('alguna-clase')
+// enlace.classList.add('alguna-clase')
 
-// enlace.onclick = miFunction;
+// // enlace.onclick = miFunction;
 
-const navegacion = document.querySelector('.navegacion');
-navegacion.insertBefore(enlace, navegacion.children[1])
-// navegacion.appendChild(enlace)
+// const navegacion = document.querySelector('.navegacion');
+// navegacion.insertBefore(enlace, navegacion.children[1])
+// // navegacion.appendChild(enlace)
