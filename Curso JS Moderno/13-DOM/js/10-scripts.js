8a74aebedc3983ffa6877cccfb440e0ce6ba9468
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-31 18:02:33 -0600

    Crear elementos en DOM

--- Curso JS Moderno/13-DOM/js/10-scripts.js
+++ Curso JS Moderno/13-DOM/js/10-scripts.js
@@ -15,3 +15,36 @@
 // const navegacion = document.querySelector('.navegacion');
 // navegacion.insertBefore(enlace, navegacion.children[1])
 // // navegacion.appendChild(enlace)
+
+const parrafo1 = document.createElement('P')
+parrafo1.textContent = 'Concierto';
+parrafo1.classList.add('categoria', 'concierto');
+
+const parrafo2 = document.createElement('P')
+parrafo2.textContent = 'Concierto de weed';
+parrafo2.classList.add('titulo')
+
+const parrafo3 = document.createElement('P')
+parrafo3.textContent = '$800 por persona'
+parrafo3.classList.add('precio');
+
+const info = document.createElement('div')
+info.classList.add('info')
+info.appendChild(parrafo1)
+info.appendChild(parrafo2)
+info.appendChild(parrafo3)
+
+const imagen = document.createElement('img');
+imagen.src = 'img/hacer2.jpg'
+
+const card = document.createElement('div')
+card.classList.add('card')
+
+card.appendChild(imagen)
+card.appendChild(info)
+
+const contenedor = document.querySelector('.hacer .contenedor-cards')
+// contenedor.appendChild(card)
+contenedor.insertBefore(card,  contenedor.children[0])
+
+console.log(parrafo2)
