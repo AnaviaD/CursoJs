d1f7acdb01e186a2f6c62310ec51288e50736b9d
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-31 18:18:54 -0600

    Commnt all

--- Curso JS Moderno/13-DOM/js/11-scripts.js
+++ Curso JS Moderno/13-DOM/js/11-scripts.js
@@ -1,20 +1,20 @@
-const btnFlotante = document.querySelector('.btn-flotante')
-const footer = document.querySelector('footer')
+// const btnFlotante = document.querySelector('.btn-flotante')
+// const footer = document.querySelector('footer')
 
-btnFlotante.addEventListener('click', mostrarOcultarFooter)
+// btnFlotante.addEventListener('click', mostrarOcultarFooter)
 
 
-// puede ser btnFlotante o this 
-function mostrarOcultarFooter(){
-    if(footer.classList.contains('activo'))
-    {
-        footer.classList.remove('activo')
-        btnFlotante.classList.remove('Activo')
-        this.textContent = 'Idioma y moneda'
-    }else{
-        footer.classList.add('activo')
-        btnFlotante.classList.add('Activo')
-        this.textContent = 'X Cerrar'
-    }
-    console.log(footer.classList)
-}
+// // puede ser btnFlotante o this 
+// function mostrarOcultarFooter(){
+//     if(footer.classList.contains('activo'))
+//     {
+//         footer.classList.remove('activo')
+//         btnFlotante.classList.remove('Activo')
+//         this.textContent = 'Idioma y moneda'
+//     }else{
+//         footer.classList.add('activo')
+//         btnFlotante.classList.add('Activo')
+//         this.textContent = 'X Cerrar'
+//     }
+//     console.log(footer.classList)
+// }
