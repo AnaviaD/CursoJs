a3362ce065d4d84be7fe3096110377a300a1db5c
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-02-03 12:38:55 -0600

    Finish class 5

---
+++ Curso JS Moderno/14-Eventos/js/05-scripts.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', ()=>{
+    // const scrollPX = window.scrollY;
+    // console.log(scrollPX)
+
+    const premium = document.querySelector('.premium')
+    const ubicacion = premium.getBoundingClientRect();
+
+    // console.log(ubicacion);
+
+    if (ubicacion.top < 100) {
+        console.log('Elemento visible')
+    }
+})
