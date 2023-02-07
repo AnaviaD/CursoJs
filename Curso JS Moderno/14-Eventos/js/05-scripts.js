9648d1c488ff85b534172060f89ecb2335c9582e
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-02-07 17:48:49 -0600

    Finish lesson 6

--- Curso JS Moderno/14-Eventos/js/05-scripts.js
+++ Curso JS Moderno/14-Eventos/js/05-scripts.js
@@ -1,13 +1,13 @@
-window.addEventListener('scroll', ()=>{
-    // const scrollPX = window.scrollY;
-    // console.log(scrollPX)
+// window.addEventListener('scroll', ()=>{
+//     // const scrollPX = window.scrollY;
+//     // console.log(scrollPX)
 
-    const premium = document.querySelector('.premium')
-    const ubicacion = premium.getBoundingClientRect();
+//     const premium = document.querySelector('.premium')
+//     const ubicacion = premium.getBoundingClientRect();
 
-    // console.log(ubicacion);
+//     // console.log(ubicacion);
 
-    if (ubicacion.top < 100) {
-        console.log('Elemento visible')
-    }
-})
+//     if (ubicacion.top < 100) {
+//         console.log('Elemento visible')
+//     }
+// })
--- Curso JS Moderno/14-Eventos/js/06-scripts.js
+++ Curso JS Moderno/14-Eventos/js/06-scripts.js
@@ -1,17 +1,17 @@
-// Event bubbling
+// // Event bubbling
   
-const cardDiv = document.querySelector('.card')
-const infoDiv = document.querySelector('.info')
-const titulo = document.querySelector('.titulo')
+// const cardDiv = document.querySelector('.card')
+// const infoDiv = document.querySelector('.info')
+// const titulo = document.querySelector('.titulo')
 
-cardDiv.addEventListener('click', () => {
-    console.log('click en card')
-});
+// cardDiv.addEventListener('click', () => {
+//     console.log('click en card')
+// });
 
-infoDiv.addEventListener('click', () => {
-    console.log('click en info')
-});
+// infoDiv.addEventListener('click', () => {
+//     console.log('click en info')
+// });
 
-titulo.addEventListener('click', ()=>{
-    console.log('click en titulo')
-})
+// titulo.addEventListener('click', ()=>{
+//     console.log('click en titulo')
+// })
