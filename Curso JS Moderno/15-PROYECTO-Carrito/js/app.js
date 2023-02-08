fa4f73380ed7de7b9f2b8de72a2bf530fc703b67
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-02-08 17:34:57 -0600

    finish class 118
    Everithing works untill now

--- Curso JS Moderno/15-PROYECTO-Carrito/js/app.js
+++ Curso JS Moderno/15-PROYECTO-Carrito/js/app.js
@@ -11,4 +11,26 @@ function cargaEventListeners(){
     listaCursos.addEventListener('click', agregarCurso);
 }
 
-// function agregarCurso
+function agregarCurso(e){
+    e.preventDefault();
+    if (e.target.classList.contains('agregar-carrito')) {
+        const cursoSeleccionado = e.target.parentElement.parentElement
+        console.log('Agregando al carrito...')
+        console.log(e.target.parentElement)
+        leerDatosCurso(cursoSeleccionado)
+    }
+}
+
+// Lee el contenido del HTML al que le dimos click y extrae la informacion del curso
+function leerDatosCurso(curso){
+    console.log(curso)
+
+    const infoCurso = {
+        imagen:     curso.querySelector('img').src,
+        titulo:     curso.querySelector('h4').textContent,
+        precio:     curso.querySelector('.precio span').textContent,
+        id:         curso.querySelector('a').getAttribute('data-id'),
+        cantidad:   1
+    }
+    console.log(infoCurso)
+}
