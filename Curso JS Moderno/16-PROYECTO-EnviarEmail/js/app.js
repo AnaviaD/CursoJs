ed3975ed5df9ec7ee8b22a0b7e3ae9dbe97b4143
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-02-14 17:46:25 -0600

    Finish lesson 129

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -12,6 +12,11 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 function validar(e){
+    if (e.target.value.trim() === '') {
+        console.log('Esta vacio')
+    }else{
+        console.log('Si hay algo')
+    }
     console.log('desde la funcion de validar');
     console.log(e.target.value);
 }
