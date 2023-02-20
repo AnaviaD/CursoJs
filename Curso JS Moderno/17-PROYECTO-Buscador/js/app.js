f8b4bc72d48beb69bf9c2c10d0883b423f3cf234
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-02-20 17:40:40 -0600

    Llenamos el select de los años

--- Curso JS Moderno/17-PROYECTO-Buscador/js/app.js
+++ Curso JS Moderno/17-PROYECTO-Buscador/js/app.js
@@ -1,8 +1,14 @@
 const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
+
+const max = new Date().getFullYear();
+const min = max - 10;
 
 // eventos
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarAutos()
+    mostrarAutos() // Muestra los automoviles
+
+    llenarSelect() //Llena los años del select
 });
 
 function mostrarAutos(){
@@ -16,4 +22,14 @@ function mostrarAutos(){
 
         resultado.appendChild(autoHTML);
     })
+}
+
+function llenarSelect(){
+    for(let i = max; i > min; i--)
+    {
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion);
+    }
 }
