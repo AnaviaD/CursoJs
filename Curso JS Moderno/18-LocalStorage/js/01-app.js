e3a43b3183ef46b05d1a7ff0c45cd6737b4ed25b
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-02-23 16:55:54 -0600

    Finish lesson 154

---
+++ Curso JS Moderno/18-LocalStorage/js/01-app.js
@@ -0,0 +1,16 @@
+// localStorage.setItem('nombre', 'juan')
+// sessionStorage.setItem('nombre', 'juan')
+
+// const producto = {
+//     nombre: "Monitor 24 pulgadas",
+//     precio: 300
+// }
+
+// const productosString = JSON.stringify (producto)
+// console.log(productosString)
+
+// localStorage.setItem('producto', productosString)
+
+// const meses = ['Enero', 'Febrero', 'Marzo']
+// const mesesString = JSON.stringify(meses)
+// localStorage.setItem('meses', mesesString)
