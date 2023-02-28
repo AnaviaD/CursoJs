aabe3c24133a2e0b88d4a11ef7cad16e11d088b5
Enrique Alberto Navia <anavia_n@outlook.com>
2023-02-28 17:44:31 -0600

    Destructure o esa cosa en objetos anidados

--- Curso JS Moderno/07-Objetos/js/05-app.js
+++ Curso JS Moderno/07-Objetos/js/05-app.js
@@ -1,14 +1,14 @@
-const producto = {
-    nombre: "Monitor 20 pulgadas",
-    precio: 300,
-    disponible: false,
-    informacion: {
-        peso: '1 kg',
-        medida: '1 metro',
-        fafricacion: {
-            pais: 'china'
-        }
-    }
-}
+// const producto = {
+//     nombre: "Monitor 20 pulgadas",
+//     precio: 300,
+//     disponible: false,
+//     informacion: {
+//         peso: '1 kg',
+//         medida: '1 metro',
+//         fafricacion: {
+//             pais: 'china'
+//         }
+//     }
+// }
 
-console.log(producto)
+// console.log(producto)
