25f9be00798f4645f1d81bc6dce66181fe6890ab
Enrique Alberto Navia <anavia_n@outlook.com>
2023-02-28 17:37:10 -0600

    Objetos anidaos
    nested objects

---
+++ Curso JS Moderno/07-Objetos/js/05-app.js
@@ -0,0 +1,14 @@
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 300,
+    disponible: false,
+    informacion: {
+        peso: '1 kg',
+        medida: '1 metro',
+        fafricacion: {
+            pais: 'china'
+        }
+    }
+}
+
+console.log(producto)
