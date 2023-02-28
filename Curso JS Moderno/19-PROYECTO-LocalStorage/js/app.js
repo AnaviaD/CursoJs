4e15078e3a9d27123db4701601224ba6400714b6
Enrique Alberto Navia <anavia_n@outlook.com>
2023-02-27 20:09:23 -0600

    Validation added

--- Curso JS Moderno/19-PROYECTO-LocalStorage/js/app.js
+++ Curso JS Moderno/19-PROYECTO-LocalStorage/js/app.js
@@ -14,6 +14,27 @@ function evenListeners(){
 //Funciones
 function agregarTweet(e){
     e.preventDefault();
+    const tweet = document.querySelector('#tweet').value;
 
-    console.log('Agregando tweet');
+    //Validacion
+    if (tweet === '') {
+        mostrarError('Un mensaje no puede ir vacio, animal');
+        return; // Se evita que se ejecuten mas lineas de la funcion
+    }
+}
+
+
+function mostrarError(error) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    //Insertarlo en el contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    // Elimina la alerta despues de 3 seg
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
 }
