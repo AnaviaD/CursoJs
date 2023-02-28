f95f4037cf0eb4381c1bb588965155dc9a5658e4
Enrique Alberto Navia <anavia_n@outlook.com>
2023-02-28 13:49:23 -0600

    Re- do the first 4 exercise

---
+++ Curso JS Moderno/07-Objetos/js/04-app.js
@@ -0,0 +1,14 @@
+// const producto = {
+//     nombre: "Monitor 20 pulgadas",
+//     precio: 300,
+//     disponible: false
+// }
+
+// // const nombre = producto.nombre;
+
+// console.log(producto)
+// // console.log(nombre)
+
+
+// const { nombre, inten, precio } = producto;
+// console.log(inten, nombre, precio)
