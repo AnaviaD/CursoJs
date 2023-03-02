4acb01ca22b10c67841d5ca9564fb168a4b59e44
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-01 18:03:30 -0600

    Add first Array
    Too ez

---
+++ Curso JS Moderno/08-Arrays/js/01-app.js
@@ -0,0 +1,11 @@
+const numeros = [10,20,30]
+
+const meses = ['Enero', 'Febrero', 'Marzo']
+
+
+console.log(numeros)
+console.log(meses)
+
+const deTodo = ["Hola", 10, true, 'si', null, {nombre: 'Juan', trabajo: 'Programador'}];
+
+console.log(deTodo)
