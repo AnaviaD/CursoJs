49410333094493de5f2b0f4c468adb1bfe1837bd
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-02 17:04:05 -0600

    Arrays Destructure

--- Curso JS Moderno/08-Arrays/js/08-app.js
+++ Curso JS Moderno/08-Arrays/js/08-app.js
@@ -1,16 +1,18 @@
-const carrito = []
+// const carrito = []
 
-//Definir un producto
-const producto = {
-    nombre: "Monitor 20 pulgadas",
-    precio: 400,
-    disponible: true
-}
+// //Definir un producto
+// const producto = {
+//     nombre: "Monitor 20 pulgadas",
+//     precio: 400,
+//     disponible: true
+// }
 
 
-//Destructuring con Arreglos
-const numeros = [10,20,30,40,50,60]
+// //Destructuring con Arreglos
+// const numeros = [10,20,30,40,50,60]
 
-const [primero] = numeros;
-console.log(primero);
-//El valor esta definido por la posicion
+// const [primero, , tercero, ...ultimos] = numeros;
+// console.log(primero);
+// console.log(tercero);
+// console.log(ultimos);
+// //El valor esta definido por la posicion
