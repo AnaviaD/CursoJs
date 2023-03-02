7c4c21e8dba7707132821e2b7a958715029460f2
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-01 18:24:28 -0600

    Destructuring Arrays

--- Curso JS Moderno/08-Arrays/js/07-app.js
+++ Curso JS Moderno/08-Arrays/js/07-app.js
@@ -1,30 +1,30 @@
-const carrito = []
+// const carrito = []
 
-//Definir un producto
-const producto = {
-    nombre: "Monitor 20 pulgadas",
-    precio: 400
-}
+// //Definir un producto
+// const producto = {
+//     nombre: "Monitor 20 pulgadas",
+//     precio: 400
+// }
 
-const producto02 = {
-    nombre: "Celular",
-    precio: 800
-}
+// const producto02 = {
+//     nombre: "Celular",
+//     precio: 800
+// }
 
-const producto03 = {
-    nombre: "Teclado",
-    precio: 50
-}
+// const producto03 = {
+//     nombre: "Teclado",
+//     precio: 50
+// }
 
-carrito.push(producto)
-carrito.push(producto02)
-carrito.push(producto03)
-console.table(carrito)
+// carrito.push(producto)
+// carrito.push(producto02)
+// carrito.push(producto03)
+// console.table(carrito)
 
-//Eliminar del final del arreglo
-carrito.pop()
-console.table(carrito)
+// //Eliminar del final del arreglo
+// carrito.pop()
+// console.table(carrito)
 
-//Eliminar del inicio del arreglo
-carrito.shift()
-console.table(carrito)
+// //Eliminar del inicio del arreglo
+// carrito.shift()
+// console.table(carrito)
---
+++ Curso JS Moderno/08-Arrays/js/08-app.js
@@ -0,0 +1,16 @@
+const carrito = []
+
+//Definir un producto
+const producto = {
+    nombre: "Monitor 20 pulgadas",
+    precio: 400,
+    disponible: true
+}
+
+
+//Destructuring con Arreglos
+const numeros = [10,20,30,40,50,60]
+
+const [primero] = numeros;
+console.log(primero);
+//El valor esta definido por la posicion
