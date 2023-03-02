ac071b8889fa9984729a9209c31f09d735007d46
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-01 18:19:06 -0600

    Shift para eliminar del inicio
    pop para eliminar del final del arreglo

--- Curso JS Moderno/08-Arrays/js/06-app.js
+++ Curso JS Moderno/08-Arrays/js/06-app.js
@@ -1,25 +1,25 @@
-const carrito = []
+// const carrito = []
 
-//Definir un producto
-const producto = {
-    nombre: "Monitor 20 pulgadas",
-    precio: 400
-}
+// //Definir un producto
+// const producto = {
+//     nombre: "Monitor 20 pulgadas",
+//     precio: 400
+// }
 
-const producto02 = {
-    nombre: "Celular",
-    precio: 800
-}
+// const producto02 = {
+//     nombre: "Celular",
+//     precio: 800
+// }
 
-const producto03 = {
-    nombre: "Teclado",
-    precio: 50
-}
+// const producto03 = {
+//     nombre: "Teclado",
+//     precio: 50
+// }
 
-let resultado;
+// let resultado;
 
-resultado = [...carrito, producto]
+// resultado = [...carrito, producto]
 
-resultado = [...resultado, producto02]
+// resultado = [...resultado, producto02]
 
-console.log(resultado)
+// console.log(resultado)
