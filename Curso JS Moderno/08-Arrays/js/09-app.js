0b695e2daae164beef918882f007e4cdca3b08fd
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-02 17:25:50 -0600

    foreach vs map
    Map puede asignar una variable de lo que tenga

--- Curso JS Moderno/08-Arrays/js/09-app.js
+++ Curso JS Moderno/08-Arrays/js/09-app.js
@@ -1,16 +1,16 @@
-const carrito = [
-    {nombre: 'Monitor 27 pulgadas', precio: 500},
-    {nombre: 'Television', precio: 100},
-    {nombre: 'Tablet', precio: 200},
-    {nombre: 'Audifonos', precio: 300},
-    {nombre: 'Teclado', precio: 400},
-    {nombre: 'Celular', precio: 700},
-]
+// const carrito = [
+//     {nombre: 'Monitor 27 pulgadas', precio: 500},
+//     {nombre: 'Television', precio: 100},
+//     {nombre: 'Tablet', precio: 200},
+//     {nombre: 'Audifonos', precio: 300},
+//     {nombre: 'Teclado', precio: 400},
+//     {nombre: 'Celular', precio: 700},
+// ]
 
-for(let i=0; i < carrito.length; i++){
-    console.log(`${carrito[i].nombre} - Precio: ${carrito[i].precio}`);
-}
+// for(let i=0; i < carrito.length; i++){
+//     console.log(`${carrito[i].nombre} - Precio: ${carrito[i].precio}`);
+// }
 
-carrito.forEach(function(produc){
-    console.log(`${produc.nombre} - Precio: ${produc.precio}`);
-})
+// carrito.forEach(function(produc){
+//     console.log(`${produc.nombre} - Precio: ${produc.precio}`);
+// })
