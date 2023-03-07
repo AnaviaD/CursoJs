567f1afafac2c9e0160d31be084e2afd777a1480
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-07 13:28:41 -0600

    Finish date lesson

---
+++ Curso JS Moderno/21-Fechas/js/01-app.js
@@ -0,0 +1,26 @@
+// const diaHoy = new Date('1993-05-03');
+// const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+
+// let valor;
+
+// valor = diaHoy
+// console.log(valor)
+// valor = diaHoy.getFullYear()
+// console.log(valor)
+
+// valor = diaHoy.getMonth()
+// console.log(valor)
+// console.log(meses[valor])
+
+// //No es lo mismo solicitar un Date()
+// //Que  solicitar un Date.now()
+
+// //Fecha de locale String 
+// const fecha001 = new Date();
+// const fechaLS = fecha001.toLocaleString()
+// console.log(fechaLS)
+
+// //Fecha de Locale Time String
+// const fecha002 = new Date();
+// const fechaLT = fecha002.toLocaleTimeString() 
+// console.log(fechaLT)
