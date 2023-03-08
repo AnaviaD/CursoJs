21d39db9507b1cff64ef0066139ab42ac9d99df4
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-08 09:31:13 -0600

    Add switch that calculate the price

--- Curso JS Moderno/23-PROYECTO-Prototypes-Seguro/js/app.js
+++ Curso JS Moderno/23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -1,88 +1,135 @@
-// //Constructores
-// function Seguro(marca, year, tipo){
-//     this.marca = marca;
-//     this.year = year;
-//     this.tipo = tipo;
-// }
+//Constructores
+function Seguro(marca, year, tipo){
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
+}
 
-// function UI(){
+Seguro.prototype.cotizarSeguro = function(){
+    /*
+        1 = Americano 1.15
+        2 = Asiatico 1.05
+        3 = Europeo
+    */
+   let cantidad
+   const base = 2000
+   console.log(this.marca)
+   switch(this.marca){
+    case '1':
+        cantidad = base * 1.15
+        break;
+    case '2':
+        cantidad = base * 1.05
+        break;
+    case '3':
+        cantidad = base * 1.35
+        break;
+    default:
+        break;
+   }
 
-// }
+   //Leer anyo
+   const diferencia = new Date().getFullYear() - this.year;
+   //Cada anyo que la diferencia es mayor, el costo va a reducirse un 3%
+   cantidad -= ((diferencia * 3) * cantidad) / 100;
+   
+   /*
+    Si el seguro es basico se multiplica por un 30%
+    Si el seguro completo se multiplica por un 50%
+   */
 
-// //Llena las opciones de los años
-// UI.prototype.llenarOpciones = () =>{
-//     const max = new Date().getFullYear(),
-//     min = max - 20;
+    if (this.tipo == 'basico') {
+        cantidad *= 1.30;
+    }else{
+        cantidad *= 1.50;
+    }
 
-//     const selectYear = document.querySelector('#year');
+    console.log(cantidad)
+    return cantidad;
+}
 
-//     for (let i = max; i > min; i--)
-//     {
-//         let option = document.createElement('option');
-//         option.value = i; 
-//         option.textContent = i;
-//         selectYear.appendChild(option);
-//     }
-// }
+function UI(){
 
-// //Muestra alertas en pantalla 
-// UI.prototype.mostrarMensajes = (mensaje, tipo) =>{
-//     const div = document.createElement('div');
+}
 
-//     if (tipo == 'error') {
-//         div.classList.add('error');
-//     }else{
-//         div.classList.add('correcto');
-//     }
+//Llena las opciones de los años
+UI.prototype.llenarOpciones = () =>{
+    const max = new Date().getFullYear(),
+    min = max - 20;
 
-//     div.classList.add('mensaje', 'mt-10');
-//     div.textContent = mensaje;
+    const selectYear = document.querySelector('#year');
 
-//     //Insertar en html
-//     const formulario = document.querySelector('#cotizar-seguro');
-//     // (nodo que se desea insertar, nodo de referencia)
-//     formulario.insertBefore(div, document.querySelector('#resultado'));
+    for (let i = max; i > min; i--)
+    {
+        let option = document.createElement('option');
+        option.value = i; 
+        option.textContent = i;
+        selectYear.appendChild(option);
+    }
+}
 
-//     setTimeout(() => {
-//         div.remove()
-//     }, 3000)
-// }
+//Muestra alertas en pantalla 
+UI.prototype.mostrarMensajes = (mensaje, tipo) =>{
+    const div = document.createElement('div');
+
+    if (tipo == 'error') {
+        div.classList.add('error');
+    }else{
+        div.classList.add('correcto');
+    }
+
+    div.classList.add('mensaje', 'mt-10');
+    div.textContent = mensaje;
+
+    //Insertar en html
+    const formulario = document.querySelector('#cotizar-seguro');
+    // (nodo que se desea insertar, nodo de referencia)
+    formulario.insertBefore(div, document.querySelector('#resultado'));
+
+    setTimeout(() => {
+        div.remove()
+    }, 3000)
+}
 
 
-// //instanciar UI
-// const ui = new UI();
-// console.log(ui);
+//instanciar UI
+const ui = new UI();
+console.log(ui);
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     ui.llenarOpciones();
-// });
+document.addEventListener("DOMContentLoaded", () => {
+    ui.llenarOpciones();
+});
 
 
-// addEventListeners();
-// function addEventListeners(){
-//     const formulario = document.querySelector('#cotizar-seguro');
-//     formulario.addEventListener('submit', cotizarSeguro)
-// }
+addEventListeners();
+function addEventListeners(){
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro)
+}
 
-// function cotizarSeguro(e){
-//     e.preventDefault();
+function cotizarSeguro(e){
+    e.preventDefault();
 
-//     //Leer la marca seleccionada
-//     const marca = document.querySelector('#marca').value;
+    //Leer la marca seleccionada
+    const marca = document.querySelector('#marca').value;
 
-//     //Leer el año seleccionado
-//     const year = document.querySelector('#year').value;
+    //Leer el año seleccionado
+    const year = document.querySelector('#year').value;
 
-//     //Leer el tipo de cobertura 
-//     const tipo = document.querySelector('input[name="tipo"]:checked').value;
+    //Leer el tipo de cobertura 
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
     
-//     if (marca == '' || tipo == '' || year == '') {        
-//         ui.mostrarMensajes('Todos los campos son obligatorios', 'error')
-//         return
-//     }
-//     ui.mostrarMensajes('Cotizando..', 'exito')
+    if (marca == '' || tipo == '' || year == '') {        
+        ui.mostrarMensajes('Todos los campos son obligatorios', 'error')
+        return
+    }
+    ui.mostrarMensajes('Cotizando..', 'exito')
 
-//     //Instanciar el seguro
+    //Instanciar el seguro
+    const seguro = new Seguro(marca, year, tipo)
 
+    seguro.cotizarSeguro()
+    
+    console.log(seguro)
 
-// }
+}
