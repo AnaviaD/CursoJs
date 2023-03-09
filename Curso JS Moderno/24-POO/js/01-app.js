f4b9807e2f8927d8157a5adbce0b13ca11e73be6
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-09 16:57:49 -0600

    Add class 01

--- Curso JS Moderno/24-POO/js/01-app.js
+++ Curso JS Moderno/24-POO/js/01-app.js
@@ -1,34 +1,34 @@
-//Clas Declaration
-class Cliente{
-    constructor(nombre, saldo){
-        this.nombre = nombre
-        this.saldo = saldo
-    }
+// //Clas Declaration
+// class Cliente{
+//     constructor(nombre, saldo){
+//         this.nombre = nombre
+//         this.saldo = saldo
+//     }
 
-    mostrarInformacion(){
-        console.log(`Cliente: ${this.nombre}, tu saldo es de ${this.saldo}`) 
-    }
+//     mostrarInformacion(){
+//         console.log(`Cliente: ${this.nombre}, tu saldo es de ${this.saldo}`) 
+//     }
 
-    static bienvenida(){
-        return `Bienvenido al cajero`
-    }
-}
+//     static bienvenida(){
+//         return `Bienvenido al cajero`
+//     }
+// }
 
-const juan = new Cliente("juan juas juas", 300);
-console.log(juan)
-juan.mostrarInformacion()
-//De nuevo las funciones estaticas dentro de los objetos
-//Solo pueden ser llamadas desde la clase y no el objeto
-console.log(Cliente.bienvenida())
+// const juan = new Cliente("juan juas juas", 300);
+// console.log(juan)
+// juan.mostrarInformacion()
+// //De nuevo las funciones estaticas dentro de los objetos
+// //Solo pueden ser llamadas desde la clase y no el objeto
+// console.log(Cliente.bienvenida())
 
 
-//Class Expression
-const Cliente02 = class{
-    constructor(nombre, saldo){
-        this.nombre = nombre
-        this.saldo = saldo
-    }
-}
+// //Class Expression
+// const Cliente02 = class{
+//     constructor(nombre, saldo){
+//         this.nombre = nombre
+//         this.saldo = saldo
+//     }
+// }
 
-const pedro = new Cliente02("Pedro Pablo", 600);
-console.log(pedro)
+// const pedro = new Cliente02("Pedro Pablo", 600);
+// console.log(pedro)
