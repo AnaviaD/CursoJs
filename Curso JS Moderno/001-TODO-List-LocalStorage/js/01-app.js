5cb973e5bba826bbbb416b95c19412936a415280
Enrique Alberto Navia <anavia_n@outlook.com>
2023-03-09 00:17:51 -0600

    Add title with JS

--- Curso JS Moderno/001-TODO-List-LocalStorage/js/01-app.js
+++ Curso JS Moderno/001-TODO-List-LocalStorage/js/01-app.js
@@ -1,3 +1,19 @@
 //Creando el HTML
 const contenedorPrincipal = document.querySelector('#mainDiv')
+const card001 = document.createElement("card")
+const carContentd001 = document.createElement("card-content")
+const cardTitle001 = document.createElement("card-title")
+const H2LoginTitle001 = document.createElement("H2")
+
+//Modificando los elementos creados
+H2LoginTitle001.innerHTML = "LOGIN"
+card001.classList.add("card")
+cardTitle001.classList.add("card-title")
+
+//Añadiendo contenido a la vista 
+contenedorPrincipal.appendChild(card001)
+card001.appendChild(carContentd001)
+carContentd001.appendChild(cardTitle001)
+cardTitle001.appendChild(H2LoginTitle001)
+
 console.log(contenedorPrincipal)
