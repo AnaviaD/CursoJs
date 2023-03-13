1a2c68a98f494827c5d1fa5ab1002f91a45fac80
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-13 16:55:51 -0600

    Add verification proyect

---
+++ Curso JS Moderno/25-PROYECTO-GastoSemanal/js/app.js
@@ -0,0 +1,24 @@
+//Variables y selectores
+const formulario = document.querySelector('#agregar-gasto')
+const gastoListado = document.querySelector('#gastos ul')
+
+//Eventos
+eventListeners();
+function eventListeners(){
+    document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+}
+
+
+//Clases
+
+
+//Funciones
+function preguntarPresupuesto(){
+    const presupuestoUsuario = prompt('¿Cuanto te pagan??')
+
+    console.log(presupuestoUsuario);
+
+    if(presupuestoUsuario == '' || presupuestoUsuario == null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0){
+        window.location.reload();
+    }
+}
