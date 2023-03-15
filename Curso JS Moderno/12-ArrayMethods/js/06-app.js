9a6dca29fad6255404e382411a699f3a12e362b8
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-15 17:04:39 -0600

    Review every
    Todos los elementos del array tienen que cumplir con la condicion

--- Curso JS Moderno/12-ArrayMethods/js/06-app.js
+++ Curso JS Moderno/12-ArrayMethods/js/06-app.js
@@ -1,1 +1,30 @@
- 
+// //=====================================Every=====================================
+// //=====================================Every=====================================
+// // Revisa que todos los miembros del array cumplan con la condcion
+// // a diferencia de some que con una condicion cumplida es suficiente
+// const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio']
+
+// const carrito = [
+//     {nombre: 'Monitor 27 pulgadas', precio: 500},
+//     {nombre: 'Television', precio: 400},
+//     {nombre: 'Tablet', precio: 200},
+//     {nombre: 'Audifonos', precio: 300},
+//     {nombre: 'Teclado', precio: 70},
+//     {nombre: 'Celular', precio: 800},
+// ];
+
+
+// const resultado = meses.every((mes) =>{
+//     return mes.length > 1
+// })
+// console.log(resultado)
+
+// const resultado001 = carrito.every((merca) =>{
+//     return merca.precio >1
+// })
+// console.log(resultado001)
+
+// const resultado002 = carrito.every((merca) =>{
+//     return merca.nombre == 'Audifonos'
+// })
+// console.log(resultado002)
