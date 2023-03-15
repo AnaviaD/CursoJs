196bd31fe2d324c864ddf7fa694c324d7af689d8
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-15 17:23:08 -0600

    Add concat and ... review

--- Curso JS Moderno/12-ArrayMethods/js/07-app.js
+++ Curso JS Moderno/12-ArrayMethods/js/07-app.js
@@ -1,1 +1,23 @@
- 
+// // =================================concat=================================
+// // =================================concat=================================
+// // Cuando se copian los arrays con contat y con los ...
+// // Cuando se utiliza copiar el arra sin los ... dentro de []
+// // Se copia el arreglo como tal, no lo incorpora
+// const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio']
+// const meses2 = ['Agosto', 'Septiembre']
+// const meses3 = ['Octubre', 'Noviembre', 'Diciembre']
+
+// const carrito = [
+//     {nombre: 'Monitor 27 pulgadas', precio: 500},
+//     {nombre: 'Television', precio: 400},
+//     {nombre: 'Tablet', precio: 200},
+//     {nombre: 'Audifonos', precio: 300},
+//     {nombre: 'Teclado', precio: 70},
+//     {nombre: 'Celular', precio: 800},
+// ];
+
+// const resultado = meses.concat(meses2, meses3)
+// console.log(resultado)
+
+// const resultado001 = [...meses, ...meses2, ...meses3]
+// console.log(resultado001)
--- Curso JS Moderno/12-ArrayMethods/js/08-app.js
+++ Curso JS Moderno/12-ArrayMethods/js/08-app.js
@@ -1,1 +1,29 @@
- 
+// const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio']
+// const meses2 = ['Agosto', 'Septiembre']
+// const meses3 = ['Octubre', 'Noviembre', 'Diciembre']
+
+// const carrito = [
+//     {nombre: 'Monitor 27 pulgadas', precio: 500},
+//     {nombre: 'Television', precio: 400},
+//     {nombre: 'Tablet', precio: 200},
+//     {nombre: 'Audifonos', precio: 300},
+//     {nombre: 'Teclado', precio: 70},
+//     {nombre: 'Celular', precio: 800},
+// ];
+
+// //Spread operator
+// const meses001 = [...meses, 'Agosto']
+// console.log(meses001)
+
+// const producto = { nombre: 'Disco Duro', precio: 300}
+// const carrito2 = [...carrito, producto]
+// console.log(carrito2)
+
+// const producto01 = {nombre: 'Laptop', precio: 450}
+
+// const carrito3 = [...carrito2, producto]
+// console.log(carrito3)
+
+// const producto02 = {nombre: 'Filtro', precio: 40}
+// const carrito4 = [...carrito3, producto02]
+// console.log(carrito4)
