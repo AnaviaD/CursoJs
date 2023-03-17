de35a48cd5312eb9ee4585319f8c6c9b2e394ee0
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-17 12:50:37 -0600

    Add event listener to fill the object

---
+++ Curso JS Moderno/26-PROYECTO-AdministrarCitas/js/app.js
@@ -0,0 +1,34 @@
+// Elementos del DOM
+const mascotaInput = document.querySelector('#mascota')
+const propietarioInput = document.querySelector('#propietario')
+const telefonoInput = document.querySelector('#telefono')
+const fechaInput = document.querySelector('#fecha')
+const horaInput = document.querySelector('#hora')
+const sintomasInput = document.querySelector('#sintomas')
+
+const contenedorCitas = document.querySelector('#citas')
+const formulario = document.querySelector('#nueva-cita')
+
+eventListeners()
+function eventListeners(){
+    mascotaInput.addEventListener('input', datosCita)
+    propietarioInput.addEventListener('input', datosCita)
+    telefonoInput.addEventListener('input', datosCita)
+    fechaInput.addEventListener('input', datosCita)
+    horaInput.addEventListener('input', datosCita)
+    sintomasInput.addEventListener('input', datosCita)
+}
+
+const citaObj = {
+    mascota: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
+}
+
+function datosCita(e){
+    citaObj[e.target.name] = e.target.value;
+    console.log(citaObj)
+}
