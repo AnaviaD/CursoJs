0a58f39c0bd9aafc45dc2a100e279a37e529ee35
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-24 11:08:17 -0600

    Finish sets maps symbols

---
+++ Curso JS Moderno/27-SetsMapsSymbols/js/08-app.js
@@ -0,0 +1,48 @@
+// const ciudades = ['Londres', 'New York', 'Madrid', 'Paris']
+// const ordenes = new Set([123, 231, 131, 102])
+// const datos = new Map()
+
+// datos.set('nombre', 'Juan')
+// datos.set('profesion', 'Desarrollador Web')
+
+// // Entries
+// // =====================================
+// for (let entry of ciudades.entries()) {
+//     console.log(entry)
+// }
+
+// for (let entry of ordenes.entries()) {    
+//     console.log(entry)
+// }
+
+// for (let entry of datos.entries()) {    
+//     console.log(entry)
+// }
+
+// // Values
+// // =====================================
+// for (let entry of ciudades.values()) {
+//     console.log(entry)
+// }
+
+// for (let entry of ordenes.values()) {    
+//     console.log(entry)
+// }
+
+// for (let entry of datos.values()) {    
+//     console.log(entry)
+// }
+
+// // Keys 
+// // =====================================
+// for (let entry of ciudades.keys()) {
+//     console.log(entry)
+// }
+
+// for (let entry of ordenes.keys()) {    
+//     console.log(entry)
+// }
+
+// for (let entry of datos.keys()) {    
+//     console.log(entry)
+// }
