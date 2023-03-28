47bc9e8841d9492ed2d56d2812a43261ea7b7e65
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-28 17:13:10 -0600

    Add review of POO

--- Curso JS Moderno/09-Funciones/js/review-app.js
+++ Curso JS Moderno/09-Funciones/js/review-app.js
@@ -1,79 +1,79 @@
 
-function funcionDeTodaLaVida(){
-    console.log('function nombreDeLaFuncion')
-}
+// function funcionDeTodaLaVida(){
+//     console.log('function nombreDeLaFuncion')
+// }
 
-const nombreDeFuncion = function(){
-    console.log('const nombreDeFuncion = function(){}')
-}
+// const nombreDeFuncion = function(){
+//     console.log('const nombreDeFuncion = function(){}')
+// }
 
-function conValoresDefault(nomre = 'nombre DEFAULT', apellido = 'apellido DEFAULT'){
-    console.log(`Hola ${nomre} ${apellido}`)
-}
+// function conValoresDefault(nomre = 'nombre DEFAULT', apellido = 'apellido DEFAULT'){
+//     console.log(`Hola ${nomre} ${apellido}`)
+// }
 
-function retornaValor(a, b){
-    return a + b;
-}
+// function retornaValor(a, b){
+//     return a + b;
+// }
 
-const reproductor = {
-    reproducir: function(id){
-        console.log(`Reproduciendo cancion con el id ${id}`)
-    },
-    pausar: function(){
-        console.log(`pausando...`)
-    }
-}
-reproductor.borrar = function(id){
-    console.log(`Borrando cancion... ${id}`)
-}
+// const reproductor = {
+//     reproducir: function(id){
+//         console.log(`Reproduciendo cancion con el id ${id}`)
+//     },
+//     pausar: function(){
+//         console.log(`pausando...`)
+//     }
+// }
+// reproductor.borrar = function(id){
+//     console.log(`Borrando cancion... ${id}`)
+// }
 
-const arrowFunction = () => {
-    console.log('primera manera de arrow Function \n const nombreFuncion = () =>{}')
-}
+// const arrowFunction = () => {
+//     console.log('primera manera de arrow Function \n const nombreFuncion = () =>{}')
+// }
 
-const aprendiendo001 = (tecnologia) => {
-    console.log(`Aprendiendo arrowFunction con parametro ${tecnologia}`)
-}
+// const aprendiendo001 = (tecnologia) => {
+//     console.log(`Aprendiendo arrowFunction con parametro ${tecnologia}`)
+// }
 
-const reproductor00112 = {
-    reproducir: id => console.log(`Reproduciendo cancion... con el ${id}`),
-    pausar: () => console.log('pausando...'),
-    borrar: id => console.log(`Borrando cancion ${id}`),
-    crearPlayList: nombre => console.log(`Creando la playlist ${nombre}`),
-    reproducirPlayList: nombre => console.log(`Reproduciendo la playlist ${nombre}`),
+// const reproductor00112 = {
+//     reproducir: id => console.log(`Reproduciendo cancion... con el ${id}`),
+//     pausar: () => console.log('pausando...'),
+//     borrar: id => console.log(`Borrando cancion ${id}`),
+//     crearPlayList: nombre => console.log(`Creando la playlist ${nombre}`),
+//     reproducirPlayList: nombre => console.log(`Reproduciendo la playlist ${nombre}`),
 
-    set nuevaCancion(cancion){
-        this.cancion = cancion;
-        console.log(`Añadiendo ${cancion}`);
-    },
-    get obtenerCancion(){
-        console.log(`${this.cancion}`)
-    }
-}
-
-
+//     set nuevaCancion(cancion){
+//         this.cancion = cancion;
+//         console.log(`Añadiendo ${cancion}`);
+//     },
+//     get obtenerCancion(){
+//         console.log(`${this.cancion}`)
+//     }
+// }
 
 
 
-funcionDeTodaLaVida()
-nombreDeFuncion()
-conValoresDefault('Juan')
-console.log(retornaValor(2, 6))
 
-reproductor.reproducir(2)
-reproductor.pausar()
-reproductor.borrar(4)
 
-arrowFunction()
+// funcionDeTodaLaVida()
+// nombreDeFuncion()
+// conValoresDefault('Juan')
+// console.log(retornaValor(2, 6))
 
-aprendiendo001('JavaScript')
+// reproductor.reproducir(2)
+// reproductor.pausar()
+// reproductor.borrar(4)
 
-reproductor00112.nuevaCancion = 'Enter Sandman'
+// arrowFunction()
 
-reproductor00112.reproducir(30)
-reproductor00112.reproducir(20)
-reproductor00112.pausar();
-reproductor00112.borrar(30)
-reproductor00112.borrar()
-reproductor00112.crearPlayList('Metal')
-reproductor00112.reproducirPlayList('Metal')
+// aprendiendo001('JavaScript')
+
+// reproductor00112.nuevaCancion = 'Enter Sandman'
+
+// reproductor00112.reproducir(30)
+// reproductor00112.reproducir(20)
+// reproductor00112.pausar();
+// reproductor00112.borrar(30)
+// reproductor00112.borrar()
+// reproductor00112.crearPlayList('Metal')
+// reproductor00112.reproducirPlayList('Metal')
--- Curso JS Moderno/24-POO/js/01-app.js
+++ Curso JS Moderno/24-POO/js/01-app.js
@@ -1,34 +1,77 @@
-// //Clas Declaration
-// class Cliente{
-//     constructor(nombre, saldo){
-//         this.nombre = nombre
-//         this.saldo = saldo
-//     }
+// ================================ Clase ================================
+// ================================ Clase ================================
+class Cliente{
 
-//     mostrarInformacion(){
-//         console.log(`Cliente: ${this.nombre}, tu saldo es de ${this.saldo}`) 
-//     }
+    // Para hacer alguna propiedad privada
+    #nombre
 
-//     static bienvenida(){
-//         return `Bienvenido al cajero`
-//     }
-// }
+    constructor(nombre, saldo){
+        this.#nombre = nombre;
+        this.saldo = saldo;
+    }
 
-// const juan = new Cliente("juan juas juas", 300);
-// console.log(juan)
-// juan.mostrarInformacion()
-// //De nuevo las funciones estaticas dentro de los objetos
-// //Solo pueden ser llamadas desde la clase y no el objeto
-// console.log(Cliente.bienvenida())
+    // Seters & Gtters
+    setNombre(nombre){
+        this.#nombre = nombre
+    }
+
+    getNombre(){
+        return this.#nombre
+    }
 
 
-// //Class Expression
-// const Cliente02 = class{
-//     constructor(nombre, saldo){
-//         this.nombre = nombre
-//         this.saldo = saldo
-//     }
-// }
+    mostrarInformacion(){
+        return `Cliente: ${this.#nombre}, tu saldo es de ${this.saldo}`
+    }
 
-// const pedro = new Cliente02("Pedro Pablo", 600);
-// console.log(pedro)
+    static bienvenida(){
+        return `Bienvenido al cajero`
+    }
+}
+
+// ============================== Herencia ==============================
+// ============================== Herencia ==============================
+class Empresa extends Cliente{
+    constructor(nombre, saldo, telefono, categoria){
+        super(nombre, saldo)
+        this.telefono = telefono;
+        this.categoria = categoria;
+    }
+// ============================== Metodo statico
+    static bienvenida(){
+        return `Bienvenido al cajero de empresas`
+    }
+}
+
+const juan = new Cliente('Juan', 200)
+const empresa = new Empresa('Codigo con Juan', 500, 5510203040, 'Aprendizaje en linea')
+
+console.log(juan.mostrarInformacion())
+console.log(juan)
+console.log(Cliente.bienvenida())
+
+console.log(empresa)
+console.log(Empresa.bienvenida())
+
+
+
+
+
+
+
+
+
+// ===============================================================================
+const Cliente02 = class{
+    constructor(nombre, saldo){
+        this.nombre = nombre;
+        this.saldo = saldo;
+    }
+    mostrarInformacion(){
+        return `Cliente: ${this.nombre}, tu saldo es de ${this.saldo}`
+    }
+}
+
+const juan2 = new Cliente02('Fernando', 400)
+console.log(juan2.mostrarInformacion())
+console.log(juan2)
