c3e245126e152efcfce98de17ea70f46a53b91df
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-28 17:33:34 -0600

    View callbacks

--- Curso JS Moderno/24-POO/js/01-app.js
+++ Curso JS Moderno/24-POO/js/01-app.js
@@ -1,59 +1,57 @@
-// ================================ Clase ================================
-// ================================ Clase ================================
-class Cliente{
+// // ================================ Clase ================================
+// // ================================ Clase ================================
+// class Cliente{
 
-    // Para hacer alguna propiedad privada
-    #nombre
+//     // Para hacer alguna propiedad privada
+//     #nombre
 
-    constructor(nombre, saldo){
-        this.#nombre = nombre;
-        this.saldo = saldo;
-    }
+//     constructor(nombre, saldo){
+//         this.#nombre = nombre;
+//         this.saldo = saldo;
+//     }
 
-    // Seters & Gtters
-    setNombre(nombre){
-        this.#nombre = nombre
-    }
+//     // Seters & Gtters
+//     setNombre(nombre){
+//         this.#nombre = nombre
+//     }
 
-    getNombre(){
-        return this.#nombre
-    }
-
-
-    mostrarInformacion(){
-        return `Cliente: ${this.#nombre}, tu saldo es de ${this.saldo}`
-    }
-
-    static bienvenida(){
-        return `Bienvenido al cajero`
-    }
-}
-
-// ============================== Herencia ==============================
-// ============================== Herencia ==============================
-class Empresa extends Cliente{
-    constructor(nombre, saldo, telefono, categoria){
-        super(nombre, saldo)
-        this.telefono = telefono;
-        this.categoria = categoria;
-    }
-// ============================== Metodo statico
-    static bienvenida(){
-        return `Bienvenido al cajero de empresas`
-    }
-}
-
-const juan = new Cliente('Juan', 200)
-const empresa = new Empresa('Codigo con Juan', 500, 5510203040, 'Aprendizaje en linea')
-
-console.log(juan.mostrarInformacion())
-console.log(juan)
-console.log(Cliente.bienvenida())
-
-console.log(empresa)
-console.log(Empresa.bienvenida())
+//     getNombre(){
+//         return this.#nombre
+//     }
 
 
+//     mostrarInformacion(){
+//         return `Cliente: ${this.#nombre}, tu saldo es de ${this.saldo}`
+//     }
+
+//     static bienvenida(){
+//         return `Bienvenido al cajero`
+//     }
+// }
+
+// // ============================== Herencia ==============================
+// // ============================== Herencia ==============================
+// class Empresa extends Cliente{
+//     constructor(nombre, saldo, telefono, categoria){
+//         super(nombre, saldo)
+//         this.telefono = telefono;
+//         this.categoria = categoria;
+//     }
+// // ============================== Metodo statico
+//     static bienvenida(){
+//         return `Bienvenido al cajero de empresas`
+//     }
+// }
+
+// const juan = new Cliente('Juan', 200)
+// const empresa = new Empresa('Codigo con Juan', 500, 5510203040, 'Aprendizaje en linea')
+
+// console.log(juan.mostrarInformacion())
+// console.log(juan)
+// console.log(Cliente.bienvenida())
+
+// console.log(empresa)
+// console.log(Empresa.bienvenida())
 
 
 
@@ -61,17 +59,19 @@ console.log(Empresa.bienvenida())
 
 
 
-// ===============================================================================
-const Cliente02 = class{
-    constructor(nombre, saldo){
-        this.nombre = nombre;
-        this.saldo = saldo;
-    }
-    mostrarInformacion(){
-        return `Cliente: ${this.nombre}, tu saldo es de ${this.saldo}`
-    }
-}
 
-const juan2 = new Cliente02('Fernando', 400)
-console.log(juan2.mostrarInformacion())
-console.log(juan2)
+
+// // ===============================================================================
+// const Cliente02 = class{
+//     constructor(nombre, saldo){
+//         this.nombre = nombre;
+//         this.saldo = saldo;
+//     }
+//     mostrarInformacion(){
+//         return `Cliente: ${this.nombre}, tu saldo es de ${this.saldo}`
+//     }
+// }
+
+// const juan2 = new Cliente02('Fernando', 400)
+// console.log(juan2.mostrarInformacion())
+// console.log(juan2)
