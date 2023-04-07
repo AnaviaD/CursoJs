ca3f32be3abf0ee6d3f71e7b04da623f7ed45e4f
Enrique Alberto Navia <anavia_n@outlook.com>
2023-04-06 21:42:15 -0600

    Promises

--- Curso JS Moderno/33-Promises/js/03-app.js
+++ Curso JS Moderno/33-Promises/js/03-app.js
@@ -1,6 +1,6 @@
 const aplicarDescuento = new Promise((resolve, reject) =>{
     
-    const descuento = true
+    const descuento = true;
 
     if (descuento) {
         resolve('Descuento Aplicado')
@@ -8,3 +8,17 @@ const aplicarDescuento = new Promise((resolve, reject) =>{
         reject('No se pudo aplicar el descuento')
     }
 })
+
+aplicarDescuento
+    .then(resultado => {
+    console.log(resultado)
+}).catch(err => {
+    console.log(err)
+})
+
+// console.log(aplicarDescuento)
+
+// Hay 3 valores posibles
+// Fullfilled - El promise se cumplio 
+// rejected - El promise no se cumplio
+// pending - No se ha cumplido ni ha sido rechazado
