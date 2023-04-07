4a381116c9782f83e3f5467eba3b0da22e41656d
Enrique Alberto Navia <anavia_n@outlook.com>
2023-04-06 21:44:17 -0600

    promises n callback

--- Curso JS Moderno/33-Promises/js/03-app.js
+++ Curso JS Moderno/33-Promises/js/03-app.js
@@ -1,24 +1,24 @@
-const aplicarDescuento = new Promise((resolve, reject) =>{
+// const aplicarDescuento = new Promise((resolve, reject) =>{
     
-    const descuento = true;
+//     const descuento = true;
 
-    if (descuento) {
-        resolve('Descuento Aplicado')
-    }else{
-        reject('No se pudo aplicar el descuento')
-    }
-})
+//     if (descuento) {
+//         resolve('Descuento Aplicado')
+//     }else{
+//         reject('No se pudo aplicar el descuento')
+//     }
+// })
 
-aplicarDescuento
-    .then(resultado => {
-    console.log(resultado)
-}).catch(err => {
-    console.log(err)
-})
+// aplicarDescuento
+//     .then(resultado => {
+//     console.log(resultado)
+// }).catch(err => {
+//     console.log(err)
+// })
 
-// console.log(aplicarDescuento)
+// // console.log(aplicarDescuento)
 
-// Hay 3 valores posibles
-// Fullfilled - El promise se cumplio 
-// rejected - El promise no se cumplio
-// pending - No se ha cumplido ni ha sido rechazado
+// // Hay 3 valores posibles
+// // Fullfilled - El promise se cumplio 
+// // rejected - El promise no se cumplio
+// // pending - No se ha cumplido ni ha sido rechazado
