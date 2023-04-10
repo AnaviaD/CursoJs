34ba7fe025340a6fd5ba30c890cb865ccbd1c22d
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-04-10 11:00:35 -0600

    Start node basics :v

---
+++ NodeJs/BasesNode/app.js
@@ -0,0 +1,11 @@
+console.clear()
+console.log('=======================================')
+console.log('========== Tabla del 5 ================')
+console.log('======================================= \n')
+
+
+let base = 5
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`${base} x ${i} = ${base*i}`)
+}
