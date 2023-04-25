0b686eb87402b4d105f97b95fcd5f155d0ea7048
Enrique Alberto Navia <anavia_n@outlook.com>
2023-04-24 21:27:28 -0600

    cargar datos del JSON

--- NodeJs/05-AplicacionConsola/app.js
+++ NodeJs/05-AplicacionConsola/app.js
@@ -1,5 +1,6 @@
 const { Tarea } = require('./models/tarea')
 const { Tareas } = require('./models/tareas')
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo')
 const { inquirerMenu,
 pausa,
 leerInput } = require('./helpers/inquirerh')
@@ -12,6 +13,13 @@ const main = async() => {
 
     let opt = ''
     const tareas = new Tareas()
+
+    const tareasDB = leerDB()
+
+    if (tareasDB) {
+        
+    }
+    await pausa()
     // const tarea = new Tarea('comprar comida')
     
     do {
@@ -33,6 +41,7 @@ const main = async() => {
                 break;
         }
 
+        // guardarDB(tareas.listadoArr)
 
         if (opt !== '0') await pausa()
         
