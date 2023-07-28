46df53704233475d36616fe1c43440ddaf6d2d54
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-07-28 11:58:42 -0600

    Get on te webService

--- NodeJs/06-ClimaApp/models/busquedas.js
+++ NodeJs/06-ClimaApp/models/busquedas.js
@@ -9,13 +9,30 @@ class Busqueda {
         //TODO: leer DB si existe
     }
 
+    get paramsMapbox(){
+        return {
+            'access_token': `pk.eyJ1IjoiZWFuYXZpYSIsImEiOiJjbGttczM4eWgwZXE4M2pueGFlcWE2c2YzIn0.3afNFrGZlGlRqgSF3d6p4g`,
+            'language': 'es'
+        }
+    }
+
     async ciudad( lugar = ''){
         
         try {
+
+            //Peticion 
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json`,
+                params: this.paramsMapbox
+            });
+
+            console.log(instance)
+            const resp = await instance.get();
+
             // peticion http
             console.log('Ciudad', lugar);
-            const resp = await axios.get('https://reqres.in/api/users?page=2')
             console.log(resp.data)
+            // console.log(resp)
         } catch (error) {
             console.log(error)            
         }
