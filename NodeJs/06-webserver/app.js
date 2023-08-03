38bcfa330c1fa5d10b3ae2bc673bfa678887b44e
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-08-03 11:48:35 -0600

    Add handlebars and example bars

--- NodeJs/06-webserver/app.js
+++ NodeJs/06-webserver/app.js
@@ -3,12 +3,28 @@ const app = express()
 
 const port = 8080;
 
+
+// TODO: require('hbs');
+app.set('view engine', 'hbs');
+
+
+//  Servir contenido estatico
+app.use(    express.static('public')    );
+
 app.get('/', (req, res) =>{
-    res.send('Hello World');
+    res.render('home')
+})
+
+app.get('/generic', (req, res) =>{
+    res.sendFile( __dirname + '/public/generic.html');
+})
+
+app.get('/elements', (req, res) =>{
+    res.sendFile( __dirname + '/public/elements.html');
 })
 
 app.get('*', (req, res) =>{
-    res.send('404 | page not found');
+    res.sendFile( __dirname + '/public/404.html');
 })
 
 app.listen( port, () =>{
