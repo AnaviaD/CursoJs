ec7eb71ade8f2d7e42ea7637a75f93a21e2dace2
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-08-14 16:33:06 -0600

    add login funcntions

--- NodeJs/07-RestServer/controllers/auth.js
+++ NodeJs/07-RestServer/controllers/auth.js
@@ -1,12 +1,50 @@
 const { response } = require("express");
+const bcryptjs = require('bcryptjs');
+
+const Usuario = require('../models/usuario')
 
 
+const login = async(req, res = response) =>{
 
-const login = (req, res = response) =>{
+    const { correo, password } = req.body;
 
-    res.json({
-        msg: 'Login ok'
-    })
+    try {
+
+        //Verificar si el email existe
+        const usr = await Usuario.findOne({ correo });
+        if ( !usr ) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - correo'
+            })
+        }
+
+        //Si el usuario esta activo
+        if ( !usr ) {
+            return res.status(400).json({
+                msg: 'estado: false'
+            })
+        }
+
+        //Verificar la contraseña
+        const validPassword = bcryptjs.compareSync( password, usr.password )
+        if ( !validPassword) {
+            return res.status(400).json({
+                msg: 'password no es correcto'
+            })
+        }
+
+        //Generar el JWT
+
+        res.json({
+            msg: 'Login ok'
+        })
+        
+    } catch (error) {
+        return res.status(500).json({
+            msg: 'Hable con el administrador'
+        })
+    }
+
 }
 
 
