2c00e1b71ed8907354b5ecf1b8c60047ff5213b9
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-08-15 16:05:58 -0600

    Finish middleware and JWT section

--- NodeJs/07-RestServer/routes/usuarios.js
+++ NodeJs/07-RestServer/routes/usuarios.js
@@ -1,9 +1,13 @@
 const { Router  } = require('express')
 const { check } = require('express-validator')
 
-const { validarCampos } = require('../middlewares/validar-campos');
-const { validarJWT } = require('../middlewares/validar-jwt');
-const { esAdminRole } = require('../middlewares/validar-roles');
+// const { validarCampos } = require('../middlewares/validar-campos');
+// const { validarJWT } = require('../middlewares/validar-jwt');
+// const { esAdminRole, tieneRole } = require('../middlewares/validar-roles');
+const {validarCampos,
+        validarJWT, 
+        esAdminRole, 
+        tieneRole} = require('../middlewares')
 
 const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
 
@@ -37,7 +41,8 @@ router.post('/',[
 
 router.delete('/:id', [
     validarJWT,
-    esAdminRole,
+    esAdminRole,    
+    tieneRole('ADMIN_ROLE', 'VENTAS_ROLE', 'NOSE_ROLE'),
     check('id', `No es un ID valido`).isMongoId(),
     check('id').custom( (id) => existeUsuarioPorId(id) ),
     validarCampos
