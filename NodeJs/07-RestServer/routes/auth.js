9d1a38865925d25868f54964410045c0872e65ec
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-08-16 11:20:44 -0600

    Add auth google controller and singIn

--- NodeJs/07-RestServer/routes/auth.js
+++ NodeJs/07-RestServer/routes/auth.js
@@ -1,7 +1,7 @@
 const { Router  } = require('express')
 const { check } = require('express-validator');
 
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 
@@ -12,6 +12,13 @@ router.post('/login', [
     check('password', 'El password debe de ser de mas de 6 letras').isLength({min: 6}),
     validarCampos
 ], login);
+
+
+router.post('/google', [
+    check('id_token', 'id_token es necesario').not().isEmpty(),
+    validarCampos
+], googleSignIn);
+
 
 
 module.exports = router;
